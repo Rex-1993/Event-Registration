3d4214945ce30ed92c44ac9a14ea69fc53341ea6
User <user@example.com>
2026-01-02 20:32:08 +0800

    Manual force deploy v1.4 (fix white screen)

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./components/ThemeProvider"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      {console.log("%c WEBSITE VERSION v1.3 LOADED ", "background: red; color: white; font-size: 20px;")}
+      {console.log("%c WEBSITE VERSION v1.4 LOADED ", "background: green; color: white; font-size: 20px;")}
       <App />
     </ThemeProvider>
   </StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: '/event-Registration/',
-
+  base: './',
 })
